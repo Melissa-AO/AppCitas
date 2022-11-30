c04f891eed4817116cb897930adec23850ce9b1d
Melissa Almeida <melissaal2000@gmail.com>
2022-11-30 03:28:45 -0600

    Lo que faltaba

--- AppCitas.Service/Startup.cs
+++ AppCitas.Service/Startup.cs
@@ -1,11 +1,5 @@
-using System.Text;
-using AppCitas.Service.Data;
 using AppCitas.Service.Extensions;
-using AppCitas.Service.Interfaces;
-using AppCitas.Service.Services;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.IdentityModel.Tokens;
+using AppCitas.Service.Middleware;
 using Microsoft.OpenApi.Models;
 
 namespace AppCitas;
@@ -38,12 +32,7 @@ public class Startup
     // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
     public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
     {
-        if (env.IsDevelopment())
-        {
-            app.UseDeveloperExceptionPage();
-            app.UseSwagger();
-            app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "WebAPIv5 v1"));
-        }
+        app.UseMiddleware<ExceptionMiddleware>();
 
         app.UseHttpsRedirection();
 
